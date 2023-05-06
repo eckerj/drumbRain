723870cbff4a303be36f84c7ad988873c55ea262
Joseph Ecker <joe.ecker@nexenta.com>
2023-05-05 19:43:23 -0500

    Updated stuff for multiplexer

--- code.py
+++ code.py
@@ -6,11 +6,38 @@ import adafruit_hid
 import time
 import struct
 
+FLOOR_SAMPLES=100
+FLOOR_SAMPLE_INTERVAL=0.001 # in seconds
+FLOOR_SAMPLE_DURATION=3 # in seconds
+
+debug=True
+
+# s0 thru s3 are multiplexer address pins.
+# s0-3 = 0,0,0,0 => multiplexer pin 0
+# s0-3 = 0,0,1,1 => multiplexer pin 3
+# etc
+s0=digitalio.DigitalInOut(board.GP18)
+s1=digitalio.DigitalInOut(board.GP19)
+s2=digitalio.DigitalInOut(board.GP20)
+s3=digitalio.DigitalInOut(board.GP21)
+
+s0.direction=Direction.OUTPUT
+s1.direction=Direction.OUTPUT
+s2.direction=Direction.OUTPUT
+s3.direction=Direction.OUTPUT
+
+# for debug purposes, we're only going to test out pin 3 (fourth input pin)
+s0.value=True
+s1.value=True
+s2.value=False
+s3.value=False
+
 #from drumbRain import DrumbRain
 
-drumbRain2 = adafruit_hid.find_device(usb_hid.devices, usage_page=0x1, usage=0x5)
+drumbRain = adafruit_hid.find_device(usb_hid.devices, usage_page=0x1, usage=0x5)
+padin = analogio.AnalogIn(board.A0)
 
-print("drumbRain device:",drumbRain2)
+if debug: print("drumbRain device:",drumbRain)
 
 def send_drumbRain_report(button_state):
     report = bytearray(2)  ## must be same size as specified in HID Report Descriptor in boot.py
@@ -21,11 +48,11 @@ def send_drumbRain_report(button_state):
         0,
         button_state
     )
-    print(["%02x" % x for x in report])
+    if debug: print(["%02x" % x for x in report])
     try:
-        drumbRain2.send_report(report)
+        drumbRain.send_report(report)
     except:
-        print("Cant send report at this time")
+        if debug: print("Cant send report at this time")
 
 # utility to help us set & clear bits in the button_state
 def set_bit(v,i):
@@ -39,9 +66,21 @@ def clr_bit(v,i):
 button_state = 0x00  # holds the state of our buttons
 button_num = 0 # which button are we current toggling
 
+def find_analog_floor(input):
+    # grab 100 samples and find the max
+    maxval = 0
+    start_time=time.time()
+    curr_time=start_time
+    while (curr_time - start_time) <= FLOOR_SAMPLE_DURATION:
+        val = input.value
+        if val > maxval: maxval=val
+#        time.sleep(FLOOR_SAMPLE_INTERVAL)
+        curr_time=time.time()
+    return maxval
+
 def main_loop():
     while True:
-        print("pushing button ", (button_num+1))  # human buttons start at one not zero
+        if debug: print("pushing button ", (button_num+1))  # human buttons start at one not zero
 
         # push button
         button_state = set_bit(button_state, button_num)
@@ -49,7 +88,7 @@ def main_loop():
         send_drumbRain_report(button_state)
         time.sleep(1)
 
-        print("releasing button ", (button_num+1))  # human buttons start at one not zero
+        if debug: print("releasing button ", (button_num+1))  # human buttons start at one not zero
 
         # release button
         button_state = clr_bit(button_state, button_num)
